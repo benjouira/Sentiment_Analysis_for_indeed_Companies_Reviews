738f4b28aaaca4730e3615c1e898b3354f95f17f
Rabii <rabi3jouira@gmail.com>
2022-01-27 22:04:49 +0100

    Create indeed_companies_reviews.py

---
+++ indeed_companies_reviews.py
@@ -0,0 +1,17 @@
+from time import time
+import random
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+import spacy
+import re
+import nltk
+from spacy.lang.fr.stop_words import STOP_WORDS
+from string import punctuation
+import seaborn as sns
+from matplotlib import style
+style.use("fivethirtyeight")
+
+# ******************
