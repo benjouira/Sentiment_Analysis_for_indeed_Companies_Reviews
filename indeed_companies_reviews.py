b2d550a852a18fef77f6bba028f8df4df547f57d
Rabii <rabi3jouira@gmail.com>
2022-02-03 17:41:05 +0100

    Update indeed_companies_reviews.py

--- indeed_companies_reviews.py
+++ indeed_companies_reviews.py
@@ -212,40 +212,41 @@ print("Gaussian Naive Bayes model accuracy :", metrics.accuracy_score(y_test, y_
 
 
 # SGDClassifier
+# it will take a lot of time 
 
-import numpy as np
-from sklearn.linear_model import SGDClassifier
-from sklearn.preprocessing import StandardScaler
-from sklearn.pipeline import make_pipeline
+# import numpy as np
+# from sklearn.linear_model import SGDClassifier
+# from sklearn.preprocessing import StandardScaler
+# from sklearn.pipeline import make_pipeline
 
-# Always scale the input. The most convenient way is to use a pipeline.
-clf = make_pipeline(StandardScaler(), SGDClassifier(max_iter=1000, tol=1e-3))
-clf.fit(x_tf_idf.toarray(), y_polarity)
+# # Always scale the input. The most convenient way is to use a pipeline.
+# clf = make_pipeline(StandardScaler(), SGDClassifier(max_iter=1000, tol=1e-3))
+# clf.fit(x_tf_idf.toarray(), y_polarity)
 
-# evaluate my model trained with raiting
+# # evaluate my model trained with raiting
 
-test1 = pd.Series("this product is great")
-test2 = pd.Series("look at that shit very bad")
-test3 = pd.Series("it's normal")
+# test1 = pd.Series("this product is great")
+# test2 = pd.Series("look at that shit very bad")
+# test3 = pd.Series("it's normal")
 
-res_test1 = CountVectorizer().fit(x).transform(test1)
-res_test2 = CountVectorizer().fit(x).transform(test2)
-res_test3 = CountVectorizer().fit(x).transform(test3)
+# res_test1 = CountVectorizer().fit(x).transform(test1)
+# res_test2 = CountVectorizer().fit(x).transform(test2)
+# res_test3 = CountVectorizer().fit(x).transform(test3)
 
-print("sentence 1 : ",clf.predict(res_test1.toarray()))
-print("sentence 2 : ",clf.predict(res_test2.toarray()))
-print("sentence 3 : ",clf.predict(res_test3.toarray()))
+# print("sentence 1 : ",clf.predict(res_test1.toarray()))
+# print("sentence 2 : ",clf.predict(res_test2.toarray()))
+# print("sentence 3 : ",clf.predict(res_test3.toarray()))
 
 
 # BaggingClassifier
+# it will take a lot of time
+
+# from sklearn.svm import SVC
+# from sklearn.ensemble import BaggingClassifier 
+
+# clf = BaggingClassifier(base_estimator=SVC(), n_estimators=10, random_state=0).fit(x_tf_idf, y_polarity)
 
 
-from sklearn.svm import SVC
-from sklearn.ensemble import BaggingClassifier 
-
-clf = BaggingClassifier(base_estimator=SVC(), n_estimators=10, random_state=0).fit(x_tf_idf, y_polarity)
-
-
-print("sentence 1 : ",clf.predict(res_test1.toarray()))
-print("sentence 2 : ",clf.predict(res_test2.toarray()))
-print("sentence 3 : ",clf.predict(res_test3.toarray()))
+# print("sentence 1 : ",clf.predict(res_test1.toarray()))
+# print("sentence 2 : ",clf.predict(res_test2.toarray()))
+# print("sentence 3 : ",clf.predict(res_test3.toarray()))
